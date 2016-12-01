db91375e26d7c0e3c03908d097943699c29501b8
Rob Hitt <robhitt@gmail.com>
2016-11-30 21:45:35 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(sample_pool)
+    sample_pool.select { |sample_word| @word.split("").sort == sample_word.split("").sort }
+  end
+end
